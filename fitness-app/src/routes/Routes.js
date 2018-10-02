182be86f3da93e94c99e7c2f2e02040b3d863969
rfogari <fogari10@gmail.com>
2018-10-01 20:37:33 -0400

    Updated Routes.js

--- fitness-app/src/routes/Routes.js
+++ fitness-app/src/routes/Routes.js
@@ -6,3 +6,25 @@ import Auth from '../auth/auth';
 import history from '../history/history';
 
 const auth = new Auth();
+
+const handleAuthentication = (nextState, replace) => {
+  if(/access_token|id_token|error/.test(nextState.location.hash)) {
+    auth.handleAuthentication();
+  }
+}
+
+const Routes = () => (
+  <Router history={history} component={Home}>
+    <div>
+      <Route exact path='/' render={(props)} => <Home auth={auth} {...props} />} />
+      <Route exact path='/home' render={(props)} => <Home auth={auth} {...props} />} />
+      <Route path='/callback' render={(props) => {
+        handleAuthentication(props);
+        return <Callback {...props} />
+      }}/>
+    </div>
+    </Router>
+);
+
+
+export default Routes;
