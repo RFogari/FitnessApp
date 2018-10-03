bd549e7ece8ca53178b971c0f27497e04e284112
rfogari <fogari10@gmail.com>
2018-10-03 00:35:39 -0400

    updated history.js file

--- fitness-app/src/history/history.js
+++ fitness-app/src/history/history.js
@@ -1,5 +1,3 @@
 import createHistory from 'history/createBrowserHistory';
 
-export default createHistory({
-  basename: process.env.NODE_ENV === 'development' ? '' :'/fitness-app'
-});
+export default createHistory();
