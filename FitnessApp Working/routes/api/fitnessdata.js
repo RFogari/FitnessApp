9b6b2d780bb6b5f5a18070e8ff49501c6b803d0d
rfogari <fogari10@gmail.com>
2018-10-06 16:40:26 -0400

    updated routes/api/fitnessdata.js

--- FitnessApp Working/routes/api/fitnessdata.js
+++ FitnessApp Working/routes/api/fitnessdata.js
@@ -1,11 +1,16 @@
 const router = require("express").Router();
 const fitnessController = require("../../controllers/fitnessController");
 
+//Match based on '/api/data'
+router.route("/")
+    .get(fitnessController.findAll)
+    .post(fitnessController.create)
+
 //match based on user token ID
 router
     .route("/:id")
     .get(fitnessController.findByID)
-    .put(fitnessController.update)
-    .post(fitnessController.create);
+    .put(fitnessController.update);
+    
     
 module.exports = router;
