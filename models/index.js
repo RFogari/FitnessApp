faadbc38f7ab10fef5d5b5acf759632bb0c9daf8
rfogari <fogari10@gmail.com>
2018-10-16 19:21:22 -0400

    Adjusting folder structure.

---
+++ models/index.js
@@ -0,0 +1,4 @@
+//export object containing Fitness Activity Data
+module.exports = {
+    Data: require("./Fitness")
+};
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+
+//body-parser to handle form submission
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//express static will server public folder
+app.use(express.static("client/public"));
+
+//Routes for both API and view
+app.use(routes);
+
+//Mongo DB connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessdatadb")
+
+//API server
+app.listen(PORT, function() {
+    console.log(` ==> API server listening on PORT ${PORT}!`)
+});
