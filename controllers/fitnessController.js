e1b25e9abb2f859b6c232950440c94748f9fdfad
rfogari <fogari10@gmail.com>
2018-10-17 05:16:57 -0400

    update to fix fitnesscontroller.js

--- controllers/fitnessController.js
+++ controllers/fitnessController.js
@@ -11,11 +11,11 @@ module.exports = {
 
     find: function(req, res) {
         db.Data
-            .find( { tokenID:  "auth0|5bb8d30fea4644072b1ae394" }, {sort: { _id: -1 }})
+            .find( { tokenID:  "auth0|5bb8d30fea4644072b1ae394" })
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err))
     },
-
+    
     create: function(req, res) {
         db.Data
             .create(req.body)
